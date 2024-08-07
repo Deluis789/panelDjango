cd461b394d9eb1e16c326877450a7b840dfd5a1e
luis diaz <luiscallizaya2934@gmail.com>
2024-08-07 02:13:37 -0800

    Acabado

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    correo = models.EmailField(max_length=100, null=False)
+    telefono = models.IntegerField(null=True)
+    f_nac = models.DateField(null=True)
+    f_registro = models.DateTimeField(auto_now_add=True, null=True)
--- panel/views.py
+++ panel/views.py
@@ -1,23 +1,66 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import Usuarios
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
 
 def listar(request):
-    return render(request, 'usuarios/listar.html', {})
+    users = Usuarios.objects.all()
+    context = {'usuarios': users}
+    return render(request, 'usuarios/listar.html', context)
 
 
 def actualizar(request):
-    return render(request, 'usuarios/actualizar.html', {})
+    if request.method == 'POST':
+            #Agrego los datos
+        if  request.POST.get('id') and request.POST.get('nombre') and request.POST.get('apellido') and request.POST.get('correo') and request.POST.get('telefono') and request.POST.get('fecha_nac'):
+            user_id_old = request.POST.get('id')
+            user_old = Usuarios()
+            user_old = Usuarios.objects.get(id = user_id_old)
+            
+            user = Usuarios()
+            user.id = request.POST.get('id')
+            user.nombre = request.POST.get('nombre')
+            user.apellido = request.POST.get('apellido')
+            user.correo = request.POST.get('correo')
+            user.telefono = request.POST.get('telefono')
+            user.f_nac = request.POST.get('fecha_nac')
+            user.f_registro = user_old.f_registro
+            user.save()
+            return redirect('listar')
+    else:
+        users = Usuarios.objects.all()
+        context = {'usuarios': users}
+        return render(request, 'usuarios/actualizar.html', context)
 
 
 def agregar(request):
-    return render(request, 'usuarios/agregar.html', {})
+    if request.method == 'POST':
+        #Agrego los datos
+        if request.POST.get('nombre') and request.POST.get('apellido') and request.POST.get('correo') and request.POST.get('telefono') and request.POST.get('fecha_nac'):
+            user = Usuarios()
+            user.nombre = request.POST.get('nombre')
+            user.apellido = request.POST.get('apellido')
+            user.correo = request.POST.get('correo')
+            user.telefono = request.POST.get('telefono')
+            user.f_nac = request.POST.get('fecha_nac')
+            user.save()
+            return redirect('listar')
+    else:
+        return render(request, 'usuarios/agregar.html', {})
 
 
 def eliminar(request):
-    return render(request, 'usuarios/eliminar.html', {})
+    if request.method == 'POST':
+        if request.POST.get('id'):
+            id_borrar = request.POST.get('id')
+            tupla = Usuarios.objects.get(id = id_borrar)
+            tupla.delete()
+            return redirect('listar')
+    else:
+        users = Usuarios.objects.all()
+        context = {'usuarios': users}
+    return render(request, 'usuarios/eliminar.html', context)
 
 
